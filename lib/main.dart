9e5ff9b42eebeecf6086751f0325039ad2591c59
Adebimpe Taiwo <adebimpe@unknown.home>
2024-12-12 17:42:54 +0000

    Does something but android app doesnt work

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:intl/intl.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Clock App',
       theme: ThemeData.dark(), // Use a dark theme for better AMOLED experience
-      home: ClockScreen(),
+      home: const ClockScreen(),
     );
   }
 }
 
 class ClockScreen extends StatefulWidget {
+  const ClockScreen({super.key});
+
   @override
-  _ClockScreenState createState() => _ClockScreenState();
+  State<ClockScreen> createState() => _ClockScreenState();
 }
 
 class _ClockScreenState extends State<ClockScreen> {
@@ -30,7 +33,8 @@ class _ClockScreenState extends State<ClockScreen> {
   void initState() {
     super.initState();
     _updateTime();
-    _timer = Timer.periodic(Duration(seconds: 1), (Timer t) => _updateTime());
+    _timer =
+        Timer.periodic(const Duration(seconds: 1), (Timer t) => _updateTime());
   }
 
   void _updateTime() {
@@ -71,10 +75,6 @@ class _ClockScreenState extends State<ClockScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black, // AMOLED black background
-      appBar: AppBar(
-        title: Text('Clock App'),
-        backgroundColor: Colors.black, // Black app bar
-      ),
       body: Align(
         alignment: Alignment.centerLeft, // Align content to the left
         child: Padding(
@@ -86,18 +86,20 @@ class _ClockScreenState extends State<ClockScreen> {
             children: <Widget>[
               Text(
                 _timeString,
-                style: TextStyle(fontSize: 100), // Large font for time
+                style: const TextStyle(fontSize: 100), // Large font for time
               ),
-              SizedBox(height: 5), // Adds space between time and date
+              const SizedBox(height: 5), // Adds space between time and date
               Text(
                 _dateString,
-                style: TextStyle(fontSize: 35), // Medium font for date
+                style: const TextStyle(fontSize: 35), // Medium font for date
               ),
-              SizedBox(height: 2), // Adds space between date and quarter/year
+              const SizedBox(
+                  height: 2), // Adds space between date and quarter/year
               Text(
                 _quarterString,
-                style:
-                    TextStyle(fontSize: 25), // Medium font for quarter and year
+                style: const TextStyle(
+                    fontSize:
+                        25), // Medium font for quarter and yearhttps://github.com/Hxmbl/clock_app/new/main
               ),
             ],
           ),
